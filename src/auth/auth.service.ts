ef6ed1f6122ce152162c3651af8e57d2f075d195
Josue ortiz <ortizjd0036@gmail.com>
2024-02-01 15:48:25 -0400

    add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
+
+import * as bcryptjs from 'bcryptjs'
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+
+    constructor(
+        private readonly usersService : UsersService,
+        private readonly jwtService: JwtService
+        ){}
+
+   async register({name, email, password} : RegisterDto){
+
+    const user = await this.usersService.findOneByEmail(email)
+
+    if(user){
+        throw new BadRequestException('User already exists');
+    }
+
+    return await this.usersService.create({
+        name, 
+        email, 
+        password : await bcryptjs.hash(password , 10)
+    });
+
+   }
+
+   async login({email , password}: LoginDto){
+    const user = await this.usersService.findOneByEmail(email);
+    if(!user){
+        throw new UnauthorizedException('Email or password is wrong');
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if(!isPasswordValid){
+        throw new UnauthorizedException('Email or password is wrong');
+    }
+    return  user;
+
+   }
+
+}
